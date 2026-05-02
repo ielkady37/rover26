1342130152d97e595182bbd4b6bf0cc258b67b02
Robo-renger <roborenger72@gmail.com>
2026-05-03 02:13:31 +0300

    revert: restore standard relative glob in setup.py data_files

--- src/control/setup.py
+++ src/control/setup.py
@@ -4,8 +4,6 @@ import os
 
 package_name = 'control'
 
-_pkg_dir = os.path.dirname(os.path.abspath(__file__))
-
 setup(
     name=package_name,
     version='0.0.0',
@@ -14,8 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch',
-            glob(os.path.join(_pkg_dir, 'launch', '*.py'))),
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
