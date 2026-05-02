f9538f2a109b6206df01b9c9b6eb0998c31379c5
Robo-renger <roborenger72@gmail.com>
2026-05-03 02:12:18 +0300

    fix: use absolute path for launch glob in setup.py

--- src/control/setup.py
+++ src/control/setup.py
@@ -4,6 +4,8 @@ import os
 
 package_name = 'control'
 
+_pkg_dir = os.path.dirname(os.path.abspath(__file__))
+
 setup(
     name=package_name,
     version='0.0.0',
@@ -12,7 +14,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob('launch/*.py')),
+        ('share/' + package_name + '/launch',
+            glob(os.path.join(_pkg_dir, 'launch', '*.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
