67063b67cbfa721b2cf7bcbf0682cc8e1fbb3aaf
AMansour5 <ahmedmonsour5@icloud.com>
2026-05-02 12:48:58 +0300

    download lidar pkg

--- src/control/launch/run.launch.py
+++ src/control/launch/run.launch.py
@@ -1,5 +1,9 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
     # example_node = Node(
@@ -9,6 +13,16 @@ def generate_launch_description():
     #     name="example_node",
     # )
 
+    rplidar_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('rplidar_ros'),
+                'launch',
+                'rplidar_c1_launch.py'
+            )
+        )
+    )
     return LaunchDescription([
         # example_node,
+        rplidar_launch,
     ])
