0d5c556171d2bcd3898e735c123beabc94833b03
MohamedHossamRT <mohamedhossamelamrawi@gmail.com>
2026-05-08 16:46:18 +0300

    launch file  clean up imports

--- src/control/launch/run.launch.py
+++ src/control/launch/run.launch.py
@@ -1,9 +1,5 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
-import os
 
 def generate_launch_description():
 
@@ -52,8 +48,8 @@ def generate_launch_description():
 
     return LaunchDescription([
         joystick_node,
-        # esp_bridge_node,
         manual_navigation_node,
         autonomous_navigation_node,
-        rosbridge_server_node
+        # esp_bridge_node,
+        # rosbridge_server_node
     ])
